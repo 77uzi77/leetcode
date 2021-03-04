63dcd28f195fcacb733a838f9fb9ebaafaa5ca46
77uzi77 <55200365+77uzi77@users.noreply.github.com>
2021-03-05 00:19:50 +0800

    Update and rename NO.1 to NO.1.java

--- NO.1.java
+++ NO.1.java
@@ -1,8 +1,8 @@
-给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数 ，并返回它们的数组下标。
-你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
-你可以按任意顺序返回答案。
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数 ，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+// 你可以按任意顺序返回答案。
 
-暴力解法：
+// 暴力解法：
   class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -17,7 +17,7 @@
     }
 }
 
-利用哈希表将寻找 target - x 的时间复杂度降低到从 O(N) 降低到 O(1)
+// 利用哈希表将寻找 target - x 的时间复杂度降低到从 O(N) 降低到 O(1)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
